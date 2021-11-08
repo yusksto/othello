36fe300ec0011f939bab4fab3921e4f587758101
yusksto <yusksto@gmail.com>
2021-11-08 21:39:03 +0900

    first fixed

--- othello_ai.hpp
+++ othello_ai.hpp
@@ -8,31 +8,31 @@
 
 class othello_ai
 {
-public:
-    othello_ai(std::vector<std::vector<double>> parameter_, int mode_, int depth_min_, int time_max_); //othello_aiセット
-    std::pair<int, int> get_place_ai(std::vector<std::vector<int>> board_, int disk_); //ai設置場所取得
+    public:
+        othello_ai(std::vector<std::vector<double>> parameter_, int mode_, int depth_min_, int time_max_); //othello_aiセット
+        std::pair<int, int> get_place_ai(std::vector<std::vector<int>> board_, int disk_); //ai設置場所取得
 
-//private:
-    //変数
-    std::vector<std::vector<double>> parameter; //盤面評価パラメータ [-1:1]
-    int mode; //aiモード 1:最強 -1:最弱
-    int depth_min; //探索最低深度 [0:)
-    int time_max; //探索最大時間 [ms]
+    private:
+        //変数
+        std::vector<std::vector<double>> parameter; //盤面評価パラメータ [-1:1]
+        int mode; //aiモード 1:最強 -1:最弱
+        int depth_min; //探索最低深度 [0:)
+        int time_max; //探索最大時間 [ms]
 
-    //関数
-    std::vector<std::pair<int, int>> get_place_able(std::vector<std::vector<int>> board_, int disk_); //設置可能場所取得
-    std::vector<std::vector<int>> get_board_placed(std::vector<std::vector<int>> board_, std::pair<int, int> place_, int disk_); //設置後盤面取得
-    double alphabeta(std::vector<std::vector<int>> board_, int disk_, int depth_, clock_t time_start_, double alpha_, double beta_); //αβ探索
-    double evaluation(std::vector<std::vector<int>> board_, int disk_); //評価関数
-    int get_disks_able(std::vector<std::vector<int>> board_, int disk_); //設置可能場所数取得
-    int get_disks_all(std::vector<std::vector<int>> board_); //石の数取得
-    int get_disks(std::vector<std::vector<int>> board_, int disk_); //石の数取得
+        //関数
+        std::vector<std::pair<int, int>> get_place_able(std::vector<std::vector<int>> board_, int disk_); //設置可能場所取得
+        std::vector<std::vector<int>> get_board_placed(std::vector<std::vector<int>> board_, std::pair<int, int> place_, int disk_); //設置後盤面取得
+        double alphabeta(std::vector<std::vector<int>> board_, int disk_, int depth_, clock_t time_start_, double alpha_, double beta_); //αβ探索
+        double evaluation(std::vector<std::vector<int>> board_, int disk_); //評価関数
+        int get_disks_able(std::vector<std::vector<int>> board_, int disk_); //設置可能場所数取得
+        int get_disks_all(std::vector<std::vector<int>> board_); //石の数取得
+        int get_disks(std::vector<std::vector<int>> board_, int disk_); //石の数取得
 
-    //その他定数、活性化関数
-    const double inf = std::numeric_limits<double>::infinity();
-    double f_1(double x); //f_1[0:1], x[0:1]
-    double f_2(double x); //f_2[0:1], x[0:1]
-    double f_3(double x); //f_3[0:1], x[0:1]
+        //その他定数、活性化関数
+        const double inf = std::numeric_limits<double>::infinity();
+        double f_1(double x); //f_1[0:1], x[0:1]
+        double f_2(double x); //f_2[0:1], x[0:1]
+        double f_3(double x); //f_3[0:1], x[0:1]
 };
 
 othello_ai::othello_ai(std::vector<std::vector<double>> parameter_, int mode_, int depth_min_, int time_max_)
@@ -451,21 +451,21 @@ double othello_ai::evaluation(std::vector<std::vector<int>> board_, int disk_)
     s_1 *= f_1(f_1(pow(f_1(n), 4)));
 
     //石の数 s_2[-1:1]
-    double disks_0 = double(get_disks(board_, disk_));
-    double disks_1 = double(get_disks(board_, -disk_));
+    int disks_0 = get_disks(board_, disk_);
+    int disks_1 = get_disks(board_, -disk_);
     double s_2 = 0;
     if (disks_0 != 0 || disks_1 != 0)
     {
-        s_2 = (disks_0 - disks_1) / (disks_0 + disks_1) * (1 - f_1(f_1(pow(f_1(n), 4))));
+        s_2 = double(disks_0 - disks_1) / double(disks_0 + disks_1) * (1 - f_1(f_1(pow(f_1(n), 4))));
     }
 
     //設置可能場所数 s_3[-1:1]
-    double disks_able_0 = double(get_disks_able(board_, disk_));
-    double disks_able_1 = double(get_disks_able(board_, -disk_));
+    int disks_able_0 = get_disks_able(board_, disk_);
+    int disks_able_1 = get_disks_able(board_, -disk_);
     double s_3 = 0;
     if (disks_able_0 != 0 || disks_able_1 != 0)
     {
-        s_3 = (disks_able_0 - disks_able_1) / (disks_able_0 + disks_able_1) * f_3(n);
+        s_3 = double(disks_able_0 - disks_able_1) / double(disks_able_0 + disks_able_1) * f_3(n);
     }
 
     return s_1 + s_2 + s_3;
