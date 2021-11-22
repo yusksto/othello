452db79ad244ce8cf5635291e56f0bc76feb37ab
yusksto <yusksto@gmail.com>
2021-11-23 00:30:17 +0900

    修正

--- csv_fstream.hpp
+++ csv_fstream.hpp
@@ -6,10 +6,10 @@
 
 class csv_fstream
 {
-    public:
-        bool can_open(std::string filename_);
-        std::vector<std::vector<std::string>> input_csv(std::string filename_);
-        void output_csv(std::string filename_, std::vector<std::vector<std::string>> data_);
+public:
+    bool can_open(std::string filename_);
+    std::vector<std::vector<std::string>> input_csv(std::string filename_);
+    void output_csv(std::string filename_, std::vector<std::vector<std::string>> data_);
 };
 
 bool csv_fstream::can_open(std::string filename_)
@@ -27,10 +27,37 @@ bool csv_fstream::can_open(std::string filename_)
 
 std::vector<std::vector<std::string>> csv_fstream::input_csv(std::string filename_)
 {
+    std::ifstream ifs(filename_);
+    if (!ifs) {
+        std::vector<std::vector<std::string>> data;
+        return data;
+    }
 
+    std::vector<std::vector<std::string>> data;
+    std::string buf;
+    while (std::getline(ifs, buf)) {
+        if (buf.size() == 0) break;
+        std::istringstream iss(buf);
+        std::vector<std::string> tmp;
+        while (std::getline(iss, buf, ','))
+        {
+            tmp.push_back(buf);
+        }
+        data.push_back(tmp);
+    }
+    return data;
 }
+
+
+
 
 void csv_fstream::output_csv(std::string filename_, std::vector<std::vector<std::string>> data_)
 {
-
+    std::ofstream ofs(filename_);
+    for (int i = 0; i < data_.size(); i++) {
+        for (int j = 0; j < data_[i].size(); j++) {
+            ofs << data_[i][j] << ",";
+        }
+        ofs << std::endl;
+    }
 }
